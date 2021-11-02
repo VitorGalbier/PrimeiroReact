cc65124d82768492cd9192aaf339b15238eed0ef
Vitor Galbier <vitor.galbier@gmail.com>
2021-11-02 16:40:21 -0300

    trabalho de front3

---
+++ src/container/App.js
@@ -0,0 +1,21 @@
+import '../Style/App.css';
+
+import FunctionComponent from '../componentes/FunctionComponent';
+import ClassComponent from '../componentes/ClassComponent';
+
+function App() {
+  return (
+<div className="App">
+     <h3>Convidado:</h3>
+     <ClassComponent nome="Nicolas" estaNaLista={true} />
+     <ClassComponent nome="Pedro" estaNaLista={false} />
+     <ClassComponent nome="Carolina" estaNaLista={true} />
+     <h3>Tarefas: </h3>
+     <FunctionComponent nome="Nicolas" tarefa="batata-frita" />
+     <FunctionComponent nome="Pedro" tarefa="pizza" />
+     <FunctionComponent nome="Carolina" tarefa="bebidas" />
+   </div>
+  );
+}
+
+export default App;
